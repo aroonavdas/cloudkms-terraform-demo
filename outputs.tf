1a6bc662c09fb612c7bba80ed29f89c4d42db894
aroonavdas <85660821+aroonavdas@users.noreply.github.com>
2023-01-20 20:27:20 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -34,6 +34,18 @@ output "keys" {
   ]
 }
 
+output "keys_with_imported_material" {
+  description = "Map of key(with imported material) name => key self link."
+  value       = local.keys_by_name_with_imported_material
+
+  # The grants are important to the key be ready to use.
+  depends_on = [
+    google_kms_crypto_key_iam_binding.owners,
+    google_kms_crypto_key_iam_binding.decrypters,
+    google_kms_crypto_key_iam_binding.encrypters,
+  ]
+}
+
 output "keyring_name" {
   description = "Name of the keyring."
   value       = google_kms_key_ring.key_ring.name
